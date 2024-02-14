f1372cbf8374d7ad90166358164ff337f0b5833c
szwedkarol <karolszwed1@gmail.com>
2024-02-14 13:26:02 +0100

    Created script for data imports

--- import_data.py
+++ import_data.py
@@ -9,28 +9,38 @@
 
 import warsaw_data_api
 import datetime
+import csv
+import time
 
 _MY_API_KEY = "2620c061-1099-44d9-baab-fdc3a772ab29"  # my api key
 ztm = warsaw_data_api.ztm(apikey=_MY_API_KEY)  # pass api key
 
-#lines = ztm.get_lines_for_bus_stop_id("7009", "01")
-#print(lines)
+start_time = time.time()
 
-buses_single = ztm.get_buses_location(line="182")
-buses_all = ztm.get_buses_location()
+timespan = 3600  # 60 minutes
+time_delta_for_buses = 75  # 1 minute and 15 seconds
+update_interval = 60  # 1 minute
 
-# for bus in buses:
-#     now = datetime.datetime.now()
-#     time_diff = now - bus.time
-#     if time_diff.seconds < 60:
-#         print(bus)
+print("Starting the data import at", datetime.datetime.now())
+print("Expected to end the data import at", datetime.datetime.now() + datetime.timedelta(seconds=timespan))
 
-for bus in buses_single:
-    now = datetime.datetime.now()
-    time_diff = now - bus.time
-    if time_diff.seconds < 60:
-        print(bus, bus.time, bus.brigade, bus.vehicle_number)
+with open('Buses_location.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["lines", "latitude", "longitude", "time", "brigade", "vehicle_number"])  # write header
 
-schedule = ztm.get_bus_stop_schedule_by_name("Marszałkowska", "01", "182")
-for ride in schedule.rides:
-    print(ride)
+while True:
+    try:
+        buses_all = ztm.get_buses_location()
+        with open('Buses_location_all.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            for bus in buses_all:
+                now = datetime.datetime.now()
+                time_diff = now - bus.time
+                if time_diff.seconds < time_delta_for_buses:
+                    writer.writerow([bus.lines, bus.location.latitude, bus.location.longitude,
+                                     bus.time.time(), bus.brigade, bus.vehicle_number])
+        time.sleep(update_interval)  # wait for 1 minute
+        if time.time() - start_time > timespan:  # if 5 minutes have passed, break the loop
+            break
+    except Exception:  # if an error occurs, ignore it and try again
+        continue
--- scraps.py
+++ scraps.py
@@ -13,11 +13,11 @@ import datetime
 _MY_API_KEY = "2620c061-1099-44d9-baab-fdc3a772ab29"  # my api key
 ztm = warsaw_data_api.ztm(apikey=_MY_API_KEY)  # pass api key
 
-#lines = ztm.get_lines_for_bus_stop_id("7009", "01")
-#print(lines)
+# lines = ztm.get_lines_for_bus_stop_id("7009", "01")
+# print(lines)
 
-buses_single = ztm.get_buses_location(line="182")
-buses_all = ztm.get_buses_location()
+# buses_single = ztm.get_buses_location(line="182")
+# buses_all = ztm.get_buses_location()
 
 # for bus in buses:
 #     now = datetime.datetime.now()
@@ -25,12 +25,20 @@ buses_all = ztm.get_buses_location()
 #     if time_diff.seconds < 60:
 #         print(bus)
 
-for bus in buses_single:
-    now = datetime.datetime.now()
-    time_diff = now - bus.time
-    if time_diff.seconds < 60:
-        print(bus, bus.time, bus.brigade, bus.vehicle_number)
+# for bus in buses_single:
+#     now = datetime.datetime.now()
+#     time_diff = now - bus.time
+#     if time_diff.seconds < 60:
+#         print(bus, bus.time, bus.brigade, bus.vehicle_number)
+#
+# schedule = ztm.get_bus_stop_schedule_by_name("Marszałkowska", "01", "182")
+# for ride in schedule.rides:
+#     print(ride)
 
-schedule = ztm.get_bus_stop_schedule_by_name("Marszałkowska", "01", "182")
-for ride in schedule.rides:
-    print(ride)
+# Calculate distance between two points with given coordinates
+import geopy.distance
+
+coords_1 = (52.24194, 21.041805)  # 13:15:22
+coords_2 = (52.241966, 21.041813)  # 13:13:14
+
+print(geopy.distance.geodesic(coords_1, coords_2).km)
