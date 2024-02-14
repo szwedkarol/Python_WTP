28f089f317c67541d4ab697ba11e8c85c51cc1c8
szwedkarol <karolszwed1@gmail.com>
2024-02-14 11:01:48 +0100

    Beginning of Wednesday.

--- scraps.py
+++ scraps.py
@@ -2,7 +2,7 @@
 # 17.02.2024 r.
 # Python programming course
 # Data analysis of the public bus transit in Warsaw
-# Importing data from Warsaw Open Data API
+# Scraps of code
 
 # API key
 # 2620c061-1099-44d9-baab-fdc3a772ab29
